da447a06c65a421b8d2fe122cb7b6bee5d11614e
Matt Parrilla <matthew.parrilla@gmail.com>
2013-10-10 10:49:37 -0400

    Comment out optional base url configuration

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ freezer = Freezer(app)
 
 app.config['FREEZER_DEFAULT_MIMETYPE'] = 'text/html'
 app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
-app.config['FREEZER_BASE_URL'] = 'http://www.vpr.net/apps/test'
 
 # If project doesn't have it's own domain/subdomain, use BASE_URL
 # app.config['FREEZER_BASE_URL'] = 'http://www.example.com/not_base'
@@ -21,12 +20,6 @@ app.config['FREEZER_BASE_URL'] = 'http://www.vpr.net/apps/test'
 @app.route('/')
 def index():
     return render_template('content.html')
-
-
-@app.route('/test/work')
-def work():
-    return render_template('content.html')
-
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'build':
