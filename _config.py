7f2df3979458df73e4e3f0a9fdcb16905960de81
Matt Parrilla <matthew.parrilla@gmail.com>
2013-11-25 11:45:39 -0500

    Update settings for new configuration

--- _config.py
+++ _config.py
@@ -1,9 +1,12 @@
+# Frozen Flask
+FREEZER_DEFAULT_MIMETYPE = 'text/html'
+FREEZER_IGNORE_MIMETYPE_WARNINGS = True
+
 # Amazon S3 Settings
-AWS_KEY = 'REQUIRED'
-AWS_SECRET_KEY = 'REQUIRED'
-AWS_BUCKET = 'REQUIRED'
-AWS_DIRECTORY = ''  # Leave blank *not false* unless project not at base URL
-                    # i.e. example.com/apps/ instead of example.com/
+AWS_KEY = ''
+AWS_SECRET_KEY = ''
+AWS_BUCKET = ''
+AWS_DIRECTORY = ''  # Use if S3 bucket is not root
 
 # Cache Settings (units in seconds)
 STATIC_EXPIRES = 60 * 24 * 3600
@@ -13,3 +16,9 @@ HTML_EXPIRES = 3600
 IGNORE_DIRECTORIES = ['.git', 'venv', 'sass', 'templates']
 IGNORE_FILES = ['.DS_Store']
 IGNORE_FILE_TYPES = ['.gz', '.pyc', '.py', '.rb', '.md']
+
+if AWS_DIRECTORY:
+    BASE_URL = 'http://' + AWS_BUCKET + '/' + AWS_DIRECTORY
+    FREEZER_BASE_URL = BASE_URL
+else:
+    BASE_URL = 'http://' + AWS_BUCKET
